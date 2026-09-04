f0b4baaf56f56038a41427ad08aede54e5a30f9b
agustinarodriguezet24 <agustinarodriguez.et24@gmail.com>
2026-09-03 23:36:34 -0300

    Add files via upload

---
+++ Agenda 3/Agenda.Datos/CuentaCteDatos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Negocio;
+
+namespace Datos
+{
+    public class CuentaCteDatos
+    {
+        private string conexion = "";
+
+        public void Agregar(CuentaCte cuenta)
+        {
+            using (MySqlConnection cn = new MySqlConnection(conexion))
+            {
+                cn.Open();
+
+                string sql = @"INSERT INTO CuentaCte
+                               (Id_Agenda, FechaApertura, LimiteCredito, EstadoCredito)
+                               VALUES
+                               (@Id_Agenda, @FechaApertura, @LimiteCredito, @EstadoCredito)";
+
+                MySqlCommand cmd = new MySqlCommand(sql, cn);
+
+                cmd.Parameters.AddWithValue("@Id_Agenda", cuenta.Id_Agenda);
+                cmd.Parameters.AddWithValue("@FechaApertura", cuenta.FechaApertura);
+                cmd.Parameters.AddWithValue("@LimiteCredito", cuenta.LimiteCredito);
+                cmd.Parameters.AddWithValue("@EstadoCredito", cuenta.EstadoCredito);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Agenda 3/Agenda.Negocio/CuentaCteNegocio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Datos;
+
+namespace Negocio
+{
+    public class CuentaCteNegocio
+    {
+        private CuentaCteDatos datos = new CuentaCteDatos();
+
+        public void Agregar(CuentaCte cuenta)
+        {
+            if (cuenta.LimiteCredito < 0)
+            {
+                throw new Exception("El límite de crédito no puede ser negativo.");
+            }
+
+            if (cuenta.EstadoCredito != "Activo" &&
+                cuenta.EstadoCredito != "Suspendido")
+            {
+                throw new Exception("El estado debe ser Activo o Suspendido.");
+            }
+
+            datos.Agregar(cuenta);
+        }
+    }
+}
